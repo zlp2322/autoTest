e3ee616f0957026763569543a6f904a0cedcf9cd
用户名 <zlp2322>
2021-12-02 21:38:59 +0800

    新增文件

--- Chapter9/src/main/java/com/course/httpclient/cookies/mycookiesforget.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/mycookiesforget.java
@@ -1,14 +1,17 @@
 package com.course.httpclient.cookies;
 
 import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.util.EntityUtils;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
@@ -27,10 +30,18 @@ public class mycookiesforget {
         String testurl=this.url+bundle.getString("getCookies.url");
         HttpGet get=new HttpGet(testurl);
         //测试代码逻辑
-        HttpClient client=new DefaultHttpClient();
+        DefaultHttpClient client=new DefaultHttpClient();
         HttpResponse response=client.execute(get);
         result= EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
+        //获取cookie信息
+        CookieStore store=client.getCookieStore();
+        List<Cookie> cookieList=store.getCookies();
+        for(Cookie cookie:cookieList){
+            String value=cookie.getValue();
+            String name=cookie.getName();
+            System.out.println("cookie name="+name+"cookie value="+value);
+        }
 
 
     }
